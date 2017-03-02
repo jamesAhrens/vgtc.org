9d216ce99ec401a4eb770c87644da73ab14a75b7
Carlos Scheidegger <cscheid@cs.arizona.edu>
2017-03-02 10:44:45 -0700

    update

--- scripts/fix_file_extensions.sh
+++ scripts/fix_file_extensions.sh
@@ -12,7 +12,8 @@ executive-committee
 membership
 sitemap
 virtual-reality-technical-achievement-award
-visualization-career-award"
+visualization-career-award
+visualization-technical-achievement-award"
 
 for file in $FILELIST; do
     mv _site/${file}.html _site/${file}
