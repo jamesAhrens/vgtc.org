de5ad9ba77534f52ff1ee1f0a4ab5486a1f06d15
Carlos Scheidegger <cscheid@cs.arizona.edu>
2017-03-03 10:12:55 -0700

    fix file extension fixing

--- scripts/fix_file_extensions.sh
+++ scripts/fix_file_extensions.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # for i in `find _site/archives _site/about_us _site/about-us _site/content -name "*.html"`; do
-for i in `find _site "*.html"`; do
+for i in `find _site -name "*.html"`; do
     mv $i `echo $i | sed s/.html$//`;
 done
 
