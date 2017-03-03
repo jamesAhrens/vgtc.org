4997b04b2b428dbf32342db380e98b945c7d9f05
Carlos Scheidegger <cscheid@cs.arizona.edu>
2017-03-03 10:10:25 -0700

    more aggressive file extension fixing

--- scripts/fix_file_extensions.sh
+++ scripts/fix_file_extensions.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 
-for i in `find _site/archives _site/about_us _site/about-us _site/content -name "*.html"`; do
+# for i in `find _site/archives _site/about_us _site/about-us _site/content -name "*.html"`; do
+for i in `find _site "*.html"`; do
     mv $i `echo $i | sed s/.html$//`;
 done
 
 # Move straggling pages
 
-FILELIST="annual-meeting
-calendar
-executive-committee
-membership
-sitemap
-virtual-reality-technical-achievement-award
-visualization-career-award
-visualization-technical-achievement-award"
+# FILELIST="annual-meeting
+# calendar
+# executive-committee
+# membership
+# sitemap
+# virtual-reality-technical-achievement-award
+# visualization-career-award
+# visualization-technical-achievement-award"
 
-for file in $FILELIST; do
-    mv _site/${file}.html _site/${file}
-done
+# for file in $FILELIST; do
+#     mv _site/${file}.html _site/${file}
+# done
 
